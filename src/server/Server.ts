0cc6e011f0a068ff2ce2fb52de21e57070a5472b
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-07-13 19:46:29 -0300

    Criar rotas, recebendo dados no body da req,Tipos de parametros, status codes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { router } from "./routes";
+
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("Olá, Diego !");
-});
+server.use(express.json());
+
+server.use(router);
 
 export { server };
---
+++ src/server/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send("Olá, Oi !");
+});
+
+router.post("/users", (req, res) => {
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+export { router };
