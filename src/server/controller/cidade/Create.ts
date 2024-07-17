c995bbf05aa86a666e78af070f71672dd2ab9932
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-07-17 15:27:29 -0300

    Validação do body com yup

--- src/server/controller/cidade/Create.ts
+++ src/server/controller/cidade/Create.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body.nome);
-  return res.status(StatusCodes.ACCEPTED).send("Created");
+const validationBody: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+  
+  try {
+    validateData = await validationBody.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+
+  return res.status(StatusCodes.ACCEPTED).send("Created !");
 };
